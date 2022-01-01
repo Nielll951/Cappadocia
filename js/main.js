680f2cf4dee4633915733f43ffa7be9756c98d3d
Kostya <rodina9180gaocheng@ukr.net>
2022-01-01 21:50:20 +0200

    finished adaptability

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(function(){
    $('.fairy-tail__slider, .our-trip__slider').slick({
       prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
       nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
-      autoplay: false,
+      autoplay: true,
       fade: true,
       responsive: [
          {
